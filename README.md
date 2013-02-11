60eaadc99a8290df079bfff7bda3f45d0a153a13
moonlimb <moonlimb@gmail.com>
2013-02-10 20:12:00 -0800

    updated run_twilio to include hello_moneky fcn

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ twilio_practice
 ===============
 
 Practice using Twilio API
+
+Files copied from:
+
+https://www.twilio.com/docs/quickstart/python/client/browser-to-browser-calls
+
+http://www.twilio.com/docs/quickstart/python/twiml/greet-caller-by-name
--- run_twilio.py
+++ run_twilio.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from twilio.util import TwilioCapability
 import twilio.twiml
-from twilio_keys import ACCOUNT_SID, AUTH_TOKEN, my_twilio_num
+from twilio_keys import ACCOUNT_SID, AUTH_TOKEN, my_twilio_num, CALLERS 
 import re
 
 app = Flask(__name__)
@@ -10,12 +10,27 @@ app = Flask(__name__)
 caller_id = "+%s" %(my_twilio_num)
 default_client = "Moon"
 
-@app.route('/voice', methods=['GET','POST']) 
+@app.route("/", methods=['GET','POST'])
+def hello_monkey():
+    # Get the caller's phone number from the incoming Twilio request
+    from_number = request.values.get('From', None)
+    resp = twilio.twiml.Response()
+
+    # if the caller is someone we know:
+    if from_number in CALLERS: # Greet the caller by name
+        resp.say("Hello " + CALLERS[from_number])
+    else:
+        resp.say("Hello Monkey")
+
+    return str(resp)
+
+
+@app.route('/voice', methods=['GET','POST'])
 def voice():
     from_number = request.values.get('PhoneNumber', None)
 
     resp = twilio.twiml.Response()
-    
+
     with resp.dial(callerId=caller_id) as r:
         if from_number and re.search('^[\d\(\)\- \+]+$', from_number):
             r.number(from_number)
@@ -31,13 +46,13 @@ def client():
 
     capability = TwilioCapability(ACCOUNT_SID, AUTH_TOKEN)
     application_sid = "APabe7650f654fc34655fc81ae71caa3ff"
-    application_sid2 = "AP123123"
+    application_sid2 = "AP123"
 
-    capability.allow_client_outgoing(application_sid)
+    capability.allow_client_outgoing(application_sid2)
     capability.allow_client_incoming(default_client)
 
     token = capability.generate()
-    return render_template('client.html', token=token)
+    return render_template('client.html', token=token, client_name=default_client)
 
 if __name__ == "__main__":
     app.run(debug=True)
