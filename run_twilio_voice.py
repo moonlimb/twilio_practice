313c1a0bc8b8ec7e1ef52782c6f6771f468155a4
moonlimb <moonlimb@gmail.com>
2013-02-10 19:40:58 -0800

    twilio: loading pigeons.. error. FIX

--- run_twilio_voice.py
+++ run_twilio_voice.py
@@ -1,16 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from twilio.util import TwilioCapability
-from twilio_keys import ACCOUNT_SID, AUTH_TOKEN
+import twilio.twiml
+from twilio_keys import ACCOUNT_SID, AUTH_TOKEN, my_twilio_num
+import re
 
 app = Flask(__name__)
+
+# my Twilio phone number
+caller_id = "+%s" %(my_twilio_num)
+print caller_id
+default_client = "Moon"
+
+@app.route('/voice', methods=['GET','POST']) 
+def voice():
+    from_number = request.values.get('PhoneNumber', None)
+
+    resp = twilio.twiml.Response()
+    
+    with resp.dial(callerId=caller_id) as r:
+        if from_number and re.search('^[\d\(\)\- \+]+$', from_number):
+            r.number(from_number)
+        else:
+            r.client(default_client)
+
+    return str(resp)
+
 
 @app.route('/client', methods=['GET','POST'])
 def client():
     """Respond to incoming requests."""
 
-    application_sid = "APabe7650f654fc34655fc81ae71caa3ff"
     capability = TwilioCapability(ACCOUNT_SID, AUTH_TOKEN)
-    capability.allow_client_outgoing(application_sid)
+    application_sid = "APabe7650f654fc34655fc81ae71caa3ff"
+    application_sid2 = "AP123123"
+
+    capability.allow_client_outgoing(application_sid2)
+    capability.allow_client_incoming(default_client)
     token = capability.generate()
     return render_template('client.html', token=token)
 
